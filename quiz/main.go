ce98873a68ebb3750d593a9fa6adde46c57a7807
ants <andreasvirkus@gmail.com>
2019-11-24 16:17:58 +0200

    Add separate exit func for quiz errors

--- quiz/main.go
+++ quiz/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
@@ -10,10 +11,19 @@ func main() {
 	csvFilename := flag.String("csv", "questions.csv", "A CSV source file for the questions (problem,answer)")
 
 	file, err := os.Open(*csvFilename)
-	fmt.Println("File", file, err)
 	if err != nil {
-		fmt.Printf("Could not open or parse CSV %s. Please make sure it exists\n", *csvFilename)
-		os.Exit(1)
+		exit(fmt.Sprintf("Could not find CSV %s. Please make sure it exists\n", *csvFilename))
 	}
-	_ = file
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Could not parse the CSV")
+	}
+	fmt.Println("Reading questions from", *csvFilename)
+	fmt.Println(lines)
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
