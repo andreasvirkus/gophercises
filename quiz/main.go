1ac10938c3fd456dd05f717f8fd523fd88b0ba3b
ants <andreasvirkus@gmail.com>
2019-11-24 16:34:04 +0200

    Scan for an answer and compare to expected result

--- quiz/main.go
+++ quiz/main.go
@@ -21,6 +21,36 @@ func main() {
 	}
 	fmt.Println("Reading questions from", *csvFilename)
 	fmt.Println(lines)
+	questions := parseLines(lines)
+	fmt.Println(questions)
+
+	correctAnswersCount := 0
+	for i, q := range questions {
+		fmt.Printf("Question #%d: %s?\n", i+1, q.problem)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == q.answer {
+			fmt.Println("Correct!")
+			correctAnswersCount++
+		}
+	}
+	fmt.Printf("You answered %d/%d questions correctly\n", correctAnswersCount, len(questions))
+}
+
+func parseLines(lines [][]string) []question {
+	formattedLines := make([]question, len(lines))
+	for i, line := range lines {
+		formattedLines[i] = question{
+			problem: line[0],
+			answer:  line[1],
+		}
+	}
+	return formattedLines
+}
+
+type question struct {
+	problem string
+	answer  string
 }
 
 func exit(msg string) {
